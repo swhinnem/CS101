d7151b3c1ef974fb0b25666b87831c1f80a6db84
Sarah Whinnem <sarah@betterment.com>
2014-08-10 15:38:24 -0400

    make a tic tac toe board

--- 02 - Silly/TicTacToe.java
+++ 02 - Silly/TicTacToe.java
@@ -16,6 +16,35 @@ public class TicTacToe {
 - due on weds
 
 */
+        boolean gameover;
+        String winner = null;
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Player 1, what is your name?");
+        String player1 = in.nextLine();
+
+        System.out.println("Player 2, what is your name?");
+        String player2 = in.nextLine();
+
+        System.out.println(player1 + " vs. " + player2);
+        char[][] gameboard = new char[3][3];
+
+        System.out.println("Board:");
+        System.out.println("    A | B | C ");
+        for (int row = 0; row < gameboard.length; row++ ) {
+            System.out.println("---------------");
+            System.out.print(row);
+            for (int col = 0; col < gameboard[row].length; col++) {
+                gameboard[row][col] = '_';
+                System.out.print(" | " + gameboard[row][col]);
+            }
+            System.out.println(" |");
+        }
+        System.out.println("---------------");
+
+        while (gameover = false) {
+            return;
+        }
+        System.out.println("Game over, " + player1 + " wins!");
     }
 }
